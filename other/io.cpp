17c35012f88241d8a16ec4fcc7bc668e05d0795a
Andrew Chikin <chikin.ap@phystech.edu>
2024-04-23 17:46:41 +0300

    added other/io.cpp

---
+++ other/io.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+template <class T, class IStream = std::istream>
+void Input(T& value, IStream& istream = std::cin) {
+  istream >> value;
+}
+
+template <class T, class IStream = std::istream>
+void Input(T begin, T end, IStream& istream = std::cin) {
+  for (auto i = begin; i != end; ++i) {
+    Input(*i, istream);
+  }
+}
+
+template <class... T>
+void Input(T&... args) {
+  (Input(args, std::cin), ...);
+}
+
+template <class T, class IStream = std::istream>
+T InputResult(IStream& istream = std::cin) {
+  T value;
+  Input(value, istream);
+  return value;
+}
+
+template <class T, class IStream = std::istream>
+void Input(std::vector<T>& vector, size_t size, IStream& istream = std::cin) {
+  vector.reserve(size + vector.size());
+  for (size_t i = 0; i < size; ++i) {
+    vector.emplace_back(InputResult<T>(istream));
+  }
+}
+
+template <class T, class IStream = std::istream>
+void Input(std::vector<T>& vector, IStream& istream = std::cin) {
+  T value;
+  while (std::cin >> value) {
+    vector.emplace_back(value);
+  }
+}
+
+template <class T, class IStream = std::istream>
+auto GetVector(IStream& istream = std::cin) {
+  return InputResult<std::vector<T>>(istream);
+}
