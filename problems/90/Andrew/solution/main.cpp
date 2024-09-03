0eedd33f1af073059049ea3f9c5b195f9fd13a7e
Andrew Chikin <chikin.ap@phystech.edu>
2024-09-03 17:29:34 +0300

    90: Andrew: pogress

--- problems/90/Andrew/solution/main.cpp
+++ problems/90/Andrew/solution/main.cpp
@@ -1,88 +1,91 @@
+#include <cstddef>
+#include <queue>
 #include <set>
 #include <vector>
 #include <functional>
 #include <iostream>
 
-struct Edge {
-  int64_t vrt;
-  int64_t wt;
+using NodeType = size_t;
+using WeightType = size_t;
 
-  Edge(const int64_t& vrt, const int64_t& weight) : vrt(vrt), wt(weight) {
-  }
+struct Edge {
+  NodeType from;
+  NodeType to;
+  WeightType weight;
 
   Edge() = default;
-
-  friend bool operator<(const Edge& first, const Edge& second) {
-    return ((first.wt < second.wt) || ((first.wt == second.wt) && (first.vrt < second.vrt)));
+  Edge(NodeType from, NodeType to, WeightType weight) : from(from), to(to), weight(weight) {
   }
 };
 
+// Graph: not oriented
 class Graph {
+  using AdjListT = std::vector<std::vector<Edge>>;
+
  public:
-  explicit Graph(const int64_t& new_quantity_vert) : quantity_vert_(new_quantity_vert) {
-    edg.resize(quantity_vert_ + 1);
-    dist.resize(quantity_vert_ + 1, k_max_dist);
-    prev.resize(quantity_vert_ + 1, -1);
-    used.resize(quantity_vert_ + 1, false);
+  explicit Graph(size_t n) : adj_list_(n) {
   }
 
-  void PushEdge(int64_t first_vert, int64_t second_vert, int64_t weight);
-  int64_t Prim();
-  const int64_t k_max_dist = 10000000;
-  std::vector<std::vector<Edge>> edg;
-  int64_t quantity_vert_;
-  std::vector<int64_t> prev;
-  std::vector<int64_t> dist;
-  std::vector<bool> used;
+  void AddEdge(const Edge& edge) {
+    adj_list_[edge.from].emplace_back(edge);
+    adj_list_[edge.to].emplace_back(edge.Reverse());
+  }
+
+  size_t CountNodes() const {
+    return adj_list_.size();
+  }
+
+  auto&& GetEdges(NodeType node) const {
+    return adj_list_[node];
+  }
+
+ protected:
+  AdjListT adj_list_;
 };
 
-void Graph::PushEdge(int64_t first_vert, int64_t second_vert, int64_t weight) {
-  edg[first_vert].emplace_back(second_vert, weight);
-  edg[second_vert].emplace_back(first_vert, weight);
-}
+size_t PrimAlgorithm(const Graph& graph) {
+  auto n = graph.CountNodes();
+  auto cmp = [](const Edge& first, const Edge& second) { return first.weight < second.weight; };
+  std::priority_queue<Edge, std::vector<Edge>, decltype(cmp)> queue(cmp);
+  std::vector<bool> visited(n, false);
 
-int64_t Graph::Prim() {
-  int64_t wtmst{};
-  std::set<Edge> set_vrtes;
-  dist[1] = 0;
-  for (int64_t i = 1; i <= quantity_vert_; i++) {
-    set_vrtes.insert(Edge(i, dist[i]));
-  }
-  while (!set_vrtes.empty()) {
-    auto new_vrt = *set_vrtes.begin();
-    set_vrtes.erase(set_vrtes.begin());
-    used[new_vrt.vrt] = true;
-    if (prev[new_vrt.vrt] != -1) {
-      wtmst += new_vrt.wt;
-    }
-    for (auto& edge : edg[new_vrt.vrt]) {
-      if ((!used[edge.vrt]) && (edge.wt < dist[edge.vrt])) {
-        set_vrtes.erase(Edge(edge.vrt, dist[edge.vrt]));
-        set_vrtes.insert(Edge(edge.vrt, edge.wt));
-        prev[edge.vrt] = new_vrt.vrt;
-        dist[edge.vrt] = edge.wt;
+  WeightType total_weight = 0;
+
+  // queue.push();
+  while (!queue.empty()) {
+    auto cur_edge = queue.top();
+    queue.pop();
+    visited[cur_edge.to] = true;
+    total_weight += cur_edge.weight;
+    for (auto& next_edge : graph.GetEdges(cur_edge.to)) {
+      if (!visited[next_edge.to]) {
+        queue.push(next_edge);
       }
     }
   }
-  return wtmst;
+  return total_weight;
 }
 
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
-  int64_t quantity_vrtes{};
-  int64_t quantity_edges{};
-  std::cin >> quantity_vrtes >> quantity_edges;
-  Graph graph(quantity_vrtes);
-  int64_t first_vert{};
-  int64_t second_vert{};
-  int64_t weight{};
-  for (int64_t i = 1; i <= quantity_edges; i++) {
-    std::cin >> first_vert >> second_vert >> weight;
-    graph.PushEdge(first_vert, second_vert, weight);
+
+  size_t n{};
+  size_t m{};
+  std::cin >> n >> m;
+  Graph graph(n);
+  for (size_t i = 0; i < m; i++) {
+    NodeType from{};
+    NodeType to{};
+    WeightType weight{};
+    std::cin >> from >> to >> weight;
+    --from;
+    --to;
+    graph.AddEdge({from, to, weight});
   }
-  int64_t min_weight = graph.Prim();
-  std::cout << min_weight;
+
+  auto min_weight = PrimAlgorithm(graph);
+  std::cout << min_weight << '\n';
   return 0;
 }
