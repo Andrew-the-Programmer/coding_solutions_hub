37e4176f469433542a6f134915e9bc6d5367a32a
Andrew Chikin <chikin.ap@phystech.edu>
2024-09-03 20:45:15 +0300

    91: Andrew: PASSED

--- problems/91/Andrew/solution/main.cpp
+++ problems/91/Andrew/solution/main.cpp
@@ -1,94 +1,157 @@
-#include <iostream>
-#include <set>
-#include <vector>
 #include <algorithm>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <vector>
+#include <memory>
+
+using NodeType = size_t;
+using WeightType = size_t;
+
 struct Edge {
-  int64_t fr;
-  int64_t to;
-  int64_t wght;
+  NodeType from;
+  NodeType to;
+  WeightType weight;
+
   Edge() = default;
-  Edge(const int64_t& from, const int64_t& to, const int64_t& wght) : fr(from), to(to), wght(wght) {
+  Edge(NodeType from, NodeType to, WeightType weight) : from(from), to(to), weight(weight) {
+  }
+
+  Edge Reverse() const {
+    return {to, from, weight};
   }
 };
+
+// Graph: not oriented
+class Graph {
+  using AdjListT = std::vector<std::vector<Edge>>;
+  using EdgesListT = std::vector<Edge>;
+
+ public:
+  explicit Graph(size_t n) : nodes_count(n) {
+  }
+
+  void AddEdge(const Edge& edge) {
+    edges.emplace_back(edge);
+  }
+
+  size_t CountNodes() const {
+    return nodes_count;
+  }
+
+  auto&& GetAllEdges() const {
+    return edges;
+  }
+
+ protected:
+  size_t nodes_count = 0;
+  std::vector<Edge> edges;
+};
+
 class DSU {
  public:
-  void MakeSet(int64_t vertex);
-  int64_t FindSet(int64_t vertex);
-  void Union(int64_t first_vertex, int64_t second_vertex);
-  explicit DSU(int64_t quantity_vert) {
-    prnt.resize(quantity_vert + 1);
-    rk.resize(quantity_vert + 1);
-    for (int64_t i = 0; i < quantity_vert; ++i) {
+  explicit DSU(size_t size) : size(size), parent(size), rank(size), mass(size, 1) {
+    for (NodeType i = 0; i < size; ++i) {
       MakeSet(i);
     }
   }
-  DSU() = default;
-  std::vector<int64_t> prnt;
-  std::vector<int64_t> rk;
-};
-class Graph {
+
  public:
-  explicit Graph(int64_t quantity_vertexes) {
-    dsu = DSU(quantity_vertexes);
+  void MakeSet(NodeType node) {
+    parent[node] = node;
+    rank[node] = 1;
   }
-  int64_t Kruskal();
-  void PushEdge(int64_t first_vert, int64_t second_vert, int64_t wght);
-  std::vector<Edge> ed;
-  DSU dsu;
-};
-void DSU::MakeSet(int64_t vertex) {
-  prnt[vertex] = vertex;
-  rk[vertex] = 1;
-}
-int64_t DSU::FindSet(int64_t vertex) {
-  if (vertex == prnt[vertex]) {
-    return vertex;
-  }
-  return prnt[vertex] = FindSet((prnt[vertex]));
-}
-void DSU::Union(int64_t first_vertex, int64_t second_vertex) {
-  first_vertex = FindSet(first_vertex);
-  second_vertex = FindSet(second_vertex);
-  if (rk[first_vertex] < rk[second_vertex]) {
-    prnt[first_vertex] = second_vertex;
-  } else if (rk[first_vertex] > rk[second_vertex]) {
-    prnt[second_vertex] = first_vertex;
-  } else {
-    prnt[first_vertex] = second_vertex;
-    rk[second_vertex] += 1;
-  }
-}
-void Graph::PushEdge(int64_t first_vert, int64_t second_vert, int64_t wght) {
-  ed.emplace_back(first_vert, second_vert, wght);
-}
-int64_t Graph::Kruskal() {
-  int64_t wghtmst{};
-  for (auto& edge : ed) {
-    if (dsu.FindSet(edge.fr) != dsu.FindSet(edge.to)) {
-      dsu.Union(edge.fr, edge.to);
-      wghtmst += edge.wght;
+
+  NodeType FindSet(NodeType node) {
+    if (node == parent[node]) {
+      return node;
     }
+    return parent[node] = FindSet(parent[node]);
   }
-  return wghtmst;
+
+  void Hang(NodeType node, NodeType root) {
+    // hang node to root
+    parent[node] = root;
+    mass[root] += mass[node];
+  }
+
+  bool Union(NodeType first, NodeType second) {
+    auto first_root = FindSet(first);
+    auto second_root = FindSet(second);
+    if (first_root == second_root) {
+      return false;
+    }
+    if (rank[first_root] < rank[second_root]) {
+      Hang(first_root, second_root);
+    } else if (rank[first_root] > rank[second_root]) {
+      Hang(second_root, first_root);
+    } else {
+      Hang(first_root, second_root);
+      ++rank[second_root];
+    }
+    return (mass[first_root] == size) || (mass[second_root] == size);
+  }
+
+  bool InSameSet(NodeType first, NodeType second) {
+    return FindSet(first) == FindSet(second);
+  }
+
+ public:
+  size_t size{};
+  std::vector<size_t> parent{};
+  std::vector<size_t> rank{};
+  std::vector<size_t> mass{};
+};
+
+size_t KruskalAlgorithm(const Graph& graph) {
+  auto n = graph.CountNodes();
+  auto cmp = [](const Edge& first, const Edge& second) { return first.weight > second.weight; };
+  auto& edges = graph.GetAllEdges();
+  std::priority_queue<Edge, std::vector<Edge>, decltype(cmp)> queue(edges.begin(), edges.end(), cmp);
+  DSU dsu(n);
+
+  WeightType total_weight = 0;
+
+  while (!queue.empty()) {
+    auto min_edge = queue.top();
+    queue.pop();
+    if (dsu.InSameSet(min_edge.from, min_edge.to)) {
+      continue;
+    }
+    total_weight += min_edge.weight;
+    dsu.Union(min_edge.from, min_edge.to);
+  }
+  return total_weight;
 }
-int main() {
+
+void SetIostream() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
   std::cout.setf(std::ios::fixed);
   std::cout.precision(10);
-  int64_t quantity_edges{};
-  int64_t quantity_vertexes{};
-  int64_t first_vert{};
-  int64_t second_vert{};
-  int64_t wght{};
-  std::cin >> quantity_vertexes >> quantity_edges;
-  Graph graph(quantity_vertexes);
-  for (int64_t i = 0; i < quantity_edges; i++) {
-    std::cin >> first_vert >> second_vert >> wght;
-    graph.PushEdge(first_vert, second_vert, wght);
+}
+
+int main() {
+  SetIostream();
+
+  size_t n{};
+  size_t m{};
+  std::cin >> n >> m;
+  Graph graph(n);
+  for (size_t i = 0; i < m; i++) {
+    NodeType from{};
+    NodeType to{};
+    WeightType weight{};
+    std::cin >> from >> to >> weight;
+    --from;
+    --to;
+    graph.AddEdge({from, to, weight});
   }
-  int64_t min_wght = graph.Kruskal();
-  std::cout << min_wght;
+
+  auto min_weight = KruskalAlgorithm(graph);
+  std::cout << min_weight << '\n';
   return 0;
 }
