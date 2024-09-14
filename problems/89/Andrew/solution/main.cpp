994dd9ef6bc14f374334ac0788b606d8f6c5e89f
Andrew Chikin <chikin.ap@phystech.edu>
2024-09-14 22:26:57 +0300

    89: Andrew: fixed raised issues

--- problems/89/Andrew/solution/main.cpp
+++ problems/89/Andrew/solution/main.cpp
@@ -1,9 +1,10 @@
 #include <cstddef>
+#include <cstdint>
 #include <iostream>
 #include <vector>
 
-using NodeType = size_t;
-using WeightType = size_t;
+using NodeType = uint32_t;
+using WeightType = uint32_t;
 
 struct Edge {
   NodeType from;
@@ -17,7 +18,7 @@ struct Edge {
 
 class DSU {
  public:
-  explicit DSU(size_t size) : size(size), parent(size), rank(size, 0), mass(size, 0) {
+  explicit DSU(size_t size) : size_(size), parent_(size), rank_(size, 0), mass_(size, 0) {
     for (NodeType i = 0; i < size; ++i) {
       MakeSet(i);
     }
@@ -25,52 +26,59 @@ class DSU {
 
  public:
   void MakeSet(NodeType node) {
-    parent[node] = node;
-    rank[node] = 0;
+    parent_[node] = node;
+    rank_[node] = 0;
   }
 
   NodeType FindSet(NodeType node) {
-    if (node == parent[node]) {
+    if (node == parent_[node]) {
       return node;
     }
-    return parent[node] = FindSet(parent[node]);
+    return parent_[node] = FindSet(parent_[node]);
   }
 
   void Hang(NodeType node, NodeType root, WeightType weight) {
     // hang node to root
-    parent[node] = root;
-    // std::cout << mass[root] << ' ' << mass[node] << ' ' << weight << std::endl;
-    mass[root] += mass[node] + weight;
-    // std::cout << "mass[0]: " << mass[0] << ' ' << std::endl;
+    parent_[node] = root;
+    mass_[root] += mass_[node] + weight;
   }
 
   void Union(const Edge& edge) {
     auto from = FindSet(edge.from);
     auto to = FindSet(edge.to);
     if (from == to) {
-      mass[from] += edge.weight;
+      mass_[from] += edge.weight;
       return;
     }
-    if (rank[from] >= rank[to]) {
+    if (rank_[from] >= rank_[to]) {
       Hang(to, from, edge.weight);
-      if (rank[from] == rank[to]) {
-        ++rank[from];
+      if (rank_[from] == rank_[to]) {
+        ++rank_[from];
       }
     } else {
       Hang(from, to, edge.weight);
     }
   }
 
- public:
-  size_t size{};
-  std::vector<size_t> parent{};
-  std::vector<size_t> rank{};
-  std::vector<size_t> mass{};
+  WeightType GetMass(NodeType node) {
+    return mass_[node];
+  }
+
+ protected:
+  size_t size_{};
+  std::vector<NodeType> parent_{};
+  std::vector<size_t> rank_{};
+  std::vector<WeightType> mass_{};
 };
 
-int main() {
+void SetIostream() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
+}
+
+int main() {
+  SetIostream();
+
   size_t n{};
   size_t m{};
   std::cin >> n >> m;
@@ -78,7 +86,6 @@ int main() {
   for (size_t i = 0; i < m; ++i) {
     size_t operation{};
     std::cin >> operation;
-    // std::cout <<"op: " << operation << std::endl;
     switch (operation) {
       case 1: {
         NodeType from{};
@@ -95,8 +102,7 @@ int main() {
         std::cin >> node;
         --node;
         auto root = dsu.FindSet(node);
-        // std::cout << "root: " << root << std::endl;
-        std::cout << dsu.mass[root] << std::endl;
+        std::cout << dsu.GetMass(root) << '\n';
         break;
       }
     }
